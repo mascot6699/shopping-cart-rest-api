e51b6ebb7904027987c3c6f25783c38b468af4bb
mascot6699 <umangshucool@gmail.com>
2017-07-08 07:52:51 +0530

    Adds helper method to find if user_id of logged in user from authenticated request

--- src/main/java/com/shukla/umang/service/Impl/UserServiceImpl.java
+++ src/main/java/com/shukla/umang/service/Impl/UserServiceImpl.java
@@ -1,8 +1,10 @@
 package com.shukla.umang.service.Impl;
 
+import java.security.Principal;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -11,6 +13,7 @@ import com.shukla.umang.domain.User;
 import com.shukla.umang.service.UserService;
 import com.shukla.umang.dto.request.CreateUserRequest;
 import com.shukla.umang.repository.UserRepository;
+
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -35,6 +38,14 @@ public class UserServiceImpl implements UserService {
         user.setAdmin(false);
         user.setPassword(getPasswordEncoder().encode(createUserRequest.getPassword()));
         repo.save(user);
+    }
+
+    public Long getLoggedInUserId(Principal principal) {
+        User activeUser = (User) ((Authentication) principal).getPrincipal();
+        if (activeUser != null) {
+            return activeUser.getId();
+        }
+        return null;
     }
 
 }
--- src/main/java/com/shukla/umang/service/UserService.java
+++ src/main/java/com/shukla/umang/service/UserService.java
@@ -1,8 +1,12 @@
 package com.shukla.umang.service;
 
+import java.security.Principal;
+
 import com.shukla.umang.dto.request.CreateUserRequest;
+import com.shukla.umang.domain.User;
 
 public interface UserService {
 
     void saveNormalUser(CreateUserRequest createUserRequest);
+    Long getLoggedInUserId(Principal principal);
 }
